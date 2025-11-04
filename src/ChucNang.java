ae623be6022c5810266d3f5f55f541b3d5f31f8b
Thinh2311 <nguyengiathinh231106@gmail.com>
2025-11-04 15:03:39 +0700

    Them dinh dang luu txt

--- src/ChucNang.java
+++ src/ChucNang.java
@@ -2,5 +2,6 @@ public interface ChucNang {
     public abstract void them();
     public abstract void sua();
     public abstract void xoa();
+    public abstract String chuyenSangDinhDangTXT();
     public abstract void timKiem();
 }
